26951243f6095b53b58b0ae7d9e59c920e83b906
BushenRaaj <166102383+BushenRaaj@users.noreply.github.com>
2024-07-16 17:16:02 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,146 @@
+/*const welcome={
+    fname : "bhushanraaj",
+    lname : "yougaraj",
+    name :function() {
+        console.log(this.fname + " " + this.lname);
+    }
+}
+welcome.name()
+const well={
+    city : "hosur",
+    state : "tamilnadu",
+}
+console.log(well.city+ " " + well.state);
+
+call(welcome,well)
+
+     (OR)
+
+const welcome={
+    name :function() {
+       return this.fname + " " + this.lname;
+    }
+}
+const well={
+    fname : "bhushanraaj",
+    lname : "yougaraj",
+}
+document.write(well.fname+ " " + well.lname);
+call(welcome,well)
+
+*/
+// const hello =() => document.write(" bhushanraaj");
+// hello();
+
+// const add =(a,b) => a+b;document.write(add(4,5));
+
+
+
+
+
+
+
+
+
+
+
+
+// ARRAY LIST PROGRAM 
+
+/*
+// ARRAY MANIPULATION
+// PUSH()
+
+let a=[1,2,3,4,5,6];
+a.push(10);
+document.write(a);
+
+// POP()
+
+let b=[1,2,3,4,5,6];
+b.pop(6);
+document.write(b);
+
+
+// UNSHIFT
+
+let c=[1,2,3,4,5,6];
+c.unshift(1);
+document.write(c);
+
+// SHIFT
+
+let d=[1,2,3,4,5,6];
+d.shift(1);
+document.write(d);
+*/
+
+
+/*
+
+//ARRAY SEARCHING
+
+//indexOf()
+
+let a=[1,2,3,4,5,6];
+b=a.indexOf(1);
+document.write(b);
+
+//find()
+
+// let number=[1,2,3,4,5,6];
+// document.write(number.find(6));
+
+
+//includes()
+
+let d=[1,2,3,4,5,6];
+e=d.includes(5);
+document.write(e);
+
+*/
+
+/*
+// ARRAY FILTERING
+
+let num = [1, 2, 3, 4, 5, 6];
+
+let even = num.filter(function(num) {
+  return num% 2 === 0;
+});
+
+document.write(even);
+
+*/
+
+/*
+// ARRAY SORTING
+// SORT
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.sort();
+document.write(fruits); 
+
+// REVERSE
+let num = [40, 100, 1, 5, 25, 10];
+num.reverse();
+document.write(num); 
+
+*/
+
+/*
+// ARRAY JOINING AND SPLITTING
+// join()
+
+var letters = ["a", "b", "c", "d"];
+var letter = letters.join(" ");
+document.write(letter);
+
+
+// split()
+
+let str = "A b c d e";
+let words = str.split(" ");
+document.write(words); 
+
+*/
